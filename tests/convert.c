e76c59bca723ce71c56539f19bf26d257fe411eb
fiplox <56274824+fiplox@users.noreply.github.com>
2020-04-02 04:20:35 +0200

    itoa and ftoi

---
+++ tests/convert.c
@@ -0,0 +1,86 @@
+#include "convert.h"
+
+
+
+// Reverses a string 'str' of length 'len'
+void reverse(char* str, int len)
+{
+    int i = 0, j = len - 1, temp;
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+// Converts a given integer x to string str[].
+// d is the number of digits required in the output.
+// If d is more than the number of digits in x,
+// then 0s are added at the beginning.
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x) {
+        str[i++] = (x % 10) + '0';
+        x = x / 10;
+    }
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+
+// Converts a floating-point/double number to a string.
+void ftoa(float n, char* res)
+{
+    // Extract integer part
+    int ipart = (int)n;
+
+    // Extract floating part
+    float fpart = n - (float)ipart;
+
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+
+    // check for display option after point
+
+        res[i] = '.'; // add dot
+
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter
+        // is needed to handle cases like 233.007
+        fpart = fpart * (float) pow(10, 4);
+
+
+        intToStr((int)fpart, res + i + 1, 4);
+
+}
+
+char*
+itoa(int i, char b[])
+{
+    char const digit[] = "0123456789";
+    char* p = b;
+    if(i<0){
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do{ //Move to where representation ends
+        ++p;
+        shifter = shifter/10;
+    }while(shifter);
+    *p = '\0';
+    do{ //Move back, inserting digits as u go
+        *--p = digit[i%10];
+        i = i/10;
+    }while(i);
+    return b;
+}
---
+++ tests/convert.h
@@ -0,0 +1,7 @@
+#ifndef SHELL_INTERPRETER_CONVERT_H
+#define SHELL_INTERPRETER_CONVERT_H
+#include <math.h>
+#include <stdio.h>
+void ftoa(float, char*);
+char* itoa(int,char[]);
+#endif //SHELL_INTERPRETER_CONVERT_H
